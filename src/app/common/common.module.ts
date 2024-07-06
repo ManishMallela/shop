c10da056baca72f7e65feed1eb21073abc150225
ManishMallela <mallelamanish76@gmail.com>
2024-07-06 11:08:00 +0530

    added compoents and services

---
+++ src/app/common/common.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommonComponent } from './component/common/common.component';
+
+
+
+@NgModule({
+  declarations: [
+    CommonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommonModule { }
